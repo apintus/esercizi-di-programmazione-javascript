fbb9023ff5b7e478f1747deefce84c6df62ca6b4
apintus <andre.pintus@libero.it>
2021-02-03 23:01:31 +0100

    Ok

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,17 @@
   
   http://www.imparareaprogrammare.it
 */
+
+
+var a=1;
+var b=2;
+var c=3;
+var d=4;
+var e=5
+var somma =0;
+var media =0;
+
+somma= a+b+c+d+e;
+media= somma/5
+
+console.log("somma: " + somma +" - " + " media: "+media);
