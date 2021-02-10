64b6a110089dbd6c1baf044a4d7395aa24629af0
apintus <andre.pintus@libero.it>
2021-02-10 13:54:25 +0100

    commit on 'master'

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,73 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var a,b,c,d,e,f,g;
+var maggiore=0;
+var minore=0;
+
+a = window.prompt("Numero a");
+b = window.prompt("Numero b");
+c = window.prompt("Numero c");
+d = window.prompt("Numero d");
+e = window.prompt("Numero e");
+f = window.prompt("Numero f");
+g = window.prompt("Numero g");
+
+
+if(a> maggiore){
+  maggiore = a;
+}
+else{
+  minore =a;
+}
+
+if(b> maggiore){
+  maggiore = b;
+}
+else if(b<minore){
+  minore =b;
+}
+
+if(c> maggiore){
+  maggiore = c;
+}
+else if(c<minore){
+  minore =c;
+}
+
+if(d> maggiore){
+  maggiore = d;
+}
+else if(d<minore){
+  minore =d;
+}
+
+
+if(e> maggiore){
+  maggiore = e;
+}
+else if (e<minore){
+  minore =e;
+}
+
+
+if(f> maggiore){
+  maggiore = f;
+}
+else if (f>minore){
+  minore =f;
+}
+
+if(g> maggiore){
+  g = maggiore;
+}
+else if(g<minore){
+  g =minore;
+}
+
+
+console.log("maggiore " + maggiore);
+console.log("minore " + minore);
+
